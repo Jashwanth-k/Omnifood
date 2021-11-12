6c11b4e81688b8de1a287dad9a53c75155a21eb2
Jashwanth <jashwanth328910@gmail.com>
2021-11-12 20:58:53 +0530

    adding sticky nav and fixing flexbox

--- js/script.js
+++ js/script.js
@@ -10,14 +10,13 @@ console.log(h1);
 //   h1.style.backgroundColor = "red";
 //   h1.style.padding = "5rem";
 // });
+// const root = document.querySelector(".gallery-item");
+// root.className += " close-img";
 
 // set current year
 const currYear = document.querySelector(".year");
 const getcurrYr = new Date().getFullYear();
 currYear.textContent = getcurrYr;
-
-// const root = document.querySelector(".gallery-item");
-// root.className += " close-img";
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
@@ -31,7 +30,55 @@ btnNavEl.addEventListener("click", function () {
 ///////////////////////////////////////////////////////////
 // Smooth scrolling
 const allLinks = document.querySelectorAll("a:link");
-console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const en = entries[0];
+    console.log(en);
+
+    if (en.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
